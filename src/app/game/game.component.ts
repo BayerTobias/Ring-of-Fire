34c660a5e23e5f47a665b767b2d66ff500ad0c17
BayerTobias <tbayer2@gmx.de>
2023-08-30 21:13:20 +0200

    game class added

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { Game } from '../classes/game.class';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
-  styleUrls: ['./game.component.scss']
+  styleUrls: ['./game.component.scss'],
 })
 export class GameComponent {
+  takeCardAnimation = false;
+  game: object = [];
 
+  constructor() {}
+
+  ngOnInit() {
+    this.newGame();
+  }
+
+  takeCard() {
+    this.takeCardAnimation = true;
+  }
+
+  newGame() {
+    this.game = new Game();
+    console.log(this.game);
+  }
 }
